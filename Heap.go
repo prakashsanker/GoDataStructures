0b9dad212fb41ccb52d473e573473ad677899d76
Prakash Sanker <psanker@stanford.edu>
2015-05-21 15:59:51 -0700

    trying to implement priority queue

--- Heap.go
+++ Heap.go
@@ -1,13 +1,17 @@
 package main;
 
-import "fmt" 
+import (
+		"errors"
+		"fmt"
+)
 
 func main() {
 	testSlice := []int{4,1,3,2,16,9, 10, 14, 8, 7}
-	fmt.Println(heapSort(testSlice))
-	testSlice = []int{4,4,4,4,5}
-	fmt.Println(heapSort(testSlice))
+	//fmt.Println(heapSort(testSlice))
+	h := BuildMaxHeap(testSlice)
 
+	h.increaseKey(8, 15)
+	fmt.Println(h.slice)
 
 }
 
@@ -21,13 +25,39 @@ func (h MaxHeap) size() int {
 	return h.heapSize
 }
 
-func (h MaxHeap) findMax() int{
+func (h MaxHeap) max() int{
 	return h.slice[0]
+}
+
+func (h MaxHeap) extractMax() int {
+	if h.size() < 1 {
+		err := errors.New("Heap underflow")
+		fmt.Println(err)
+	}
+	//remove and return largest element
+	largestElem := h.slice[0]
+	h.slice[0] = h.slice[h.size()]
+	h.MaxHeapify(0)
+	return largestElem
+}
+
+func (h MaxHeap) increaseKey(i int, key int) {
+	if key < h.slice[i] {
+		err := errors.New("New key smaller than current key")
+		fmt.Println(err)
+	}
+	h.slice[i] = key
+	parentVal := h.slice[(i+1)/2]
+	index := i
+	for parentVal < key && index > 0 {
+		h.slice[(i+1)/2], h.slice[i] = h.slice[i], h.slice[(i+1)/2]
+		parentVal = h.slice[(index+1)/2]
+		index = (index +1)/2
+	}
 }
 
 func heapSort(slice []int) []int {
 	h := BuildMaxHeap(slice)
-	fmt.Println(slice)
 	for i := len(h.slice) - 1; i > 0 ; i-- {
 		first := h.slice[0]
 		last := h.slice[i]
